5738c27a8578c519aef61c4515fdb67e2cccd3fe
Max Bossing <info@maxbossing.de>
2023-02-28 20:13:58 +0100

    added TotalBlockRandomizer

--- MUtils-TotalBlockRandomizer/src/main/java/de/maxbossing/mutilstotalblockrandomizer/Listeners/BlockBreakListener.java
+++ MUtils-TotalBlockRandomizer/src/main/java/de/maxbossing/mutilstotalblockrandomizer/Listeners/BlockBreakListener.java
@@ -1,2 +1,25 @@
-package de.maxbossing.mutilstotalblockrandomizer.Listeners;public class BlockBreakListener {
+package de.maxbossing.mutilstotalblockrandomizer.Listeners;
+
+import de.maxbossing.mutils.MUtils;
+import de.maxbossing.mutils.UTils.Timer;
+import org.apache.commons.lang3.RandomUtils;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.List;
+
+public class BlockBreakListener implements Listener {
+    @EventHandler
+    public void onBreak(BlockBreakEvent event) {
+        if (!Timer.getTimer().isRunning())return;
+        if (!MUtils.getInstance().getConfigValue("challenge.totalblockrandom"))return;
+        if (event.getPlayer().hasPermission("mutils.bypass"))return;
+        event.setDropItems(false);
+        Material[] mats = Material.values();
+        int i = RandomUtils.nextInt(0, mats.length - 1);
+        event.getBlock().getWorld().dropItemNaturally(event.getBlock().getLocation(), new ItemStack(mats[i]));
+    }
 }
--- MUtils-TotalBlockRandomizer/src/main/java/de/maxbossing/mutilstotalblockrandomizer/MUtilsTotalBlockRandomizer.java
+++ MUtils-TotalBlockRandomizer/src/main/java/de/maxbossing/mutilstotalblockrandomizer/MUtilsTotalBlockRandomizer.java
@@ -1,4 +1,12 @@
 package de.maxbossing.mutilstotalblockrandomizer;
 
-public class MUtilsTotalBlockRandomizer {
+import de.maxbossing.mutilstotalblockrandomizer.Listeners.BlockBreakListener;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MUtilsTotalBlockRandomizer extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(new BlockBreakListener(), this);
+    }
 }
